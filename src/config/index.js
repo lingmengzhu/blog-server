170edb2a764fcb31eb0c2571c671efa97ecac946
陈航 <chenhang@bonree.com>
2022-05-10 18:22:45 +0800

    feat(修改连接)

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 let app  = {
     // url : 'mongodb://root:zhiailuoxi0@42.192.57.35:27127',
-    url : 'mongodb://root:zhiailuoxi0@blog_db:27127',
+    url : 'mongodb://root:zhiailuoxi0@blog_db:27017',
     dbName : 'blog'
   }
   module.exports = app
