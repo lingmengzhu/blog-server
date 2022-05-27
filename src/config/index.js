b18f74a8d8ce69fca9a43d76a593dca1a1af50a0
陈航 <chenhang@bonree.com>
2022-05-27 15:33:04 +0800

    feat(配置更改)

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,9 @@
 let app = {
-  url: 'mongodb://root:zhiailuoxi0@42.192.57.35:27127',
-  // url : 'mongodb://root:zhiailuoxi0@blog_db:27017',
+  // url: 'mongodb://root:zhiailuoxi0@42.192.57.35:27127',
+  // host: 'http://localhost',
   dbName: 'blog',
-  host: 'http://localhost',
   port: 8080,
+  url : 'mongodb://root:zhiailuoxi0@blog_db:27017',
+  host: 'http://42.192.57.35',
 };
 module.exports = app;
